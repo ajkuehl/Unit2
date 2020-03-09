0c436f79766267c4415031b1e385ec95a093e602
ajkuehl <60198331+ajkuehl@users.noreply.github.com>
2020-03-08 23:09:40 -0500

    March 8 evening

--- js/mainRefactor.js
+++ js/mainRefactor.js
@@ -6,6 +6,9 @@
 
 //creation of the map object declaired globally
 var map;
+
+var dataStats = {};
+
 // declaring the minimum value globally
 var minValue;
 
@@ -40,6 +43,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png
 
 // data passed through to calcMinValue function
 function calcMinValue(data){
+// function calcstats(data){
   // create an empty array  to store data values
   var allValues = [];
   // loop through each country in data set
@@ -52,7 +56,16 @@ function calcMinValue(data){
       allValues.push(value);
     }
   }
+  // get min, max, mean stats for arrays
+  dataStats.min = Math.min(...allValues);
+  dataStats.max = Math.max(...allValues);
+
+  // calculate mean
+  var sum = allValues.reduce(function(a,b){return a+b;});
+  dataStats.mean = sum/allValues.length;
+
   // get minimum value of our array as a starting point to display data
+  // .........................old code .............................
   var minValue = Math.min(...allValues)
   return minValue;
 }
@@ -189,6 +202,7 @@ function createSequenceControls(attributes){
 
 // UPDATE THIS >>>..........................................................................................................
 // reference https://cartographicperspectives.org/index.php/journal/article/view/cp76-donohue-et-al/1307
+// Create Legend control, passing through attributes
 function createLegend(attributes){
   var LegendControl = L.Control.extend({
     options: {
@@ -197,61 +211,52 @@ function createLegend(attributes){
     onAdd: function() {
       // create the control container with a particular class name
       var container = L.DomUtil.create('div', 'legend-control-container');
+      // Initializing other Dom elements
 
-      // add temportal legend div to container
-      $(container).append('<div id="temporal-legend">')
-
-
-
-
-
-      // ...............adding temporal index.................... unsure if correct
       // here I want to add a popup with an index of the date, function updateLegend (written into update propSymbols) will update index year)
-      $(container).append('<input class =')
+      // create range input element(year index) using jquery to select the div panel appending the slider to the div
+      // $(container).append('<input class ="year-index"type="text">');.....................
+
+      // add temportal legend div to container.................... is popup correct?
+      $(container).append('<div id="temporal-legend">');
+      $(container).append(updatePropSymbols.popupContent);
 
 
 
 
       // step 1: start  attribute legend svg String
-      var svg = '<svg id="attribute-legend" width="130px" height="130px">';
+      // var svg = '<svg id="attribute-legend" class=" " width="130px" height="130px">';
+      var svg = '<svg id="attribute-legend" width="200px" height="120px">';
       // array of circles names to base loop on
       var circles = ["max", "mean", "min"];
       // Loop to add each circle adn text to svg String
       for (var i=0; i<circles.length; i++){
+        // assign the r and cy attributes
+        var radius = calcPropRadius(dataStats[circles[i]]);
+        var cy = 100 - radius;
         // circle String
-        svg += '<circle class ="legend-circle" id="' + circles[i] + '" fill="#7932a8" fill-opacity ="0.8" stroke="#000000" cx="65"/>';
+        svg += '<circle class ="legend-circle" id="' + circles[i] + '" r="' + radius + '"cy="' + cy + '" fill="#7932a8" fill-opacity ="0.8" stroke="#000000" cx="35"/>';
+
+        // evenly space out labels
+        var textY = i *20 +54;
+
+        // test string
+        svg += '<text id="' + circles[i] + '-text" x="70" y="' + textY + '">' + Math.round(dataStats[circles[i]]*100)/100 + " thousand" + '</text>';
       };
       // clsoe svg String
       svg += "</svg>";
       // add attribute legend svg to container
       $(container).append(svg);
-
-      // PUT SCRIPT TO CREATE THE TEMPORAL LEGEND here I think i want this to  just tell me the year right now.
-
-
-      // initializing other Dom elements
-      // create range input element(slider) using jquery to select the div panel appending the slider to the div
-      // $(container).append('<input class="range-slider"type="range">');
-      //
-      // // Create step buttons using jquery to select div panel and append method to add button to the div
-      // $(container).append('<button class="step" id="reverse" title="Reverse">Reverse</button>');
-      // $(container).append('<button class="step" id="forward" title="Forward">Forward</button>');
-
-
-
-
       // disable any mouse event lisenters for the container.................................................add
-      L.DomEvent.disableClickPropagation(container);
+      // L.DomEvent.disableClickPropagation(container);
       return container;
     }
   });
   map.addControl(new LegendControl());
 
 
-  // Input listners here
-  // $('')
-
-
+  // Input listners for updating temporal legend "number of turistis in (year)"....................Not sure what to do here
+  $('.temporal-legend').append('<h2> id="legendYear"> # of tourists in (year)</h2>');
 
   // Calling the updateLegend function and passing through attributes indexed for sliding action
   // need to add an updateLegend function to update PropSymbols function
@@ -277,6 +282,7 @@ function updatePropSymbols(attribute){
     };
   });
 };
+
 
 
 // consolidating redundant code
@@ -323,6 +329,10 @@ function getData(map){
       var attributes = processData(response);
       // minValue needs to stay defined as used in calcMinValue function
       minValue = calcMinValue(response);
+
+      // getting rid of minValue takes away my proportional symbols.
+      // calcStats(response);
+
       // passing attributes to the two functions below
       createPropSymbols(response,attributes);
       createSequenceControls(attributes);
@@ -330,6 +340,27 @@ function getData(map){
     }
   });
 };
+
+// new code
+// function getData(map){
+//   // load data
+//   $.getJSON("data/NonResidentArrivals.geojson", function(response){
+//       // define attributes with what's going on in the processData function above
+//       // basially creation of an empty array to hold attributes from data
+//       var attributes = processData(response);
+//       calcStats(response);
+//
+//
+//
+//       // passing attributes to the two functions below
+//       createPropSymbols(response,attributes);
+//       createSequenceControls(attributes);
+//       createLegend(attributes);
+//     }
+//   });
+// };
+
+
 
 
 // Ready, get set, view!
